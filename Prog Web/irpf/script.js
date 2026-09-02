54ebf0e4c1cd9571409a8a7d5248dc9ac6674492
Echo <78942378+Echothefoxx@users.noreply.github.com>
2026-09-02 14:10:42 +0000

    fixes irpf

--- Prog Web/irpf/script.js
+++ Prog Web/irpf/script.js
@@ -1,53 +1,53 @@
 function calc() {
 let saldo = Number(document.getElementById("sal").value);
-let salario;
+let salario = 0;
 
-saldo = calcInit(saldo, document.getElementById("children").value, document.getElementById("misc").value);
+saldo = calcInit(saldo, document.getElementById("children").value || 0, document.getElementById("misc").value || 0);
 
 switch (true) {
-    case (base > 4664.68):
+    case (saldo > 4664.68):
         salario = faixa275(saldo);
         break;
-    case (base > 3751.06):
+    case (saldo > 3751.06):
         salario = faixa225(saldo);
         break;
-    case (base > 2826.66):
+    case (saldo > 2826.66):
         salario = faixa15(saldo);
         break;
-    case (base > 2112):
+    case (saldo > 2112):
         salario = faixa75(saldo);
         break;
-    case (base < 0):
-        base = 0;
-        desc = 0;
+    case (saldo < 0):
+        saldo = 0;
         break;
     default:
         salario = faixa0(saldo);
         break;
     }
 
-    let desc = calcDesc(salario, saldo);
+    let desc = calcDesc(saldo, salario);
 
     document.getElementById("basef").textContent = salario;
+    document.getElementById("baseini").textContent = saldo;
     document.getElementById("divida").textContent = desc;
     document.getElementById("faixa").textContent = calcAliquota(salario, desc);
 }
 
 function clean() {
     //reiniciando campos
-    document.getElementById("sal").textContent = "";
-    document.getElementById("children").textContent = "";
-    document.getElementById("misc").textContent = "";
+    document.getElementById("sal").value = "";
+    document.getElementById("children").value = "";
+    document.getElementById("misc").value = "";
 
     // reiniciando resultados
-    document.getElementById("basef").textContent = "";
-    document.getElementById("divida").textContent = "";
-    document.getElementById("faixa").textContent = "";
+    document.getElementById("basef").value = "";
+    document.getElementById("divida").value = "";
+    document.getElementById("faixa").value = "";
 }
 
 function calcAliquota(salario, desconto) {
-    if (desconto != 0) {
-        let aliq = desconto / salario;
+    if (desconto > 0) {
+        let aliq = (desconto / salario) * 100;
         return Math.round(aliq * 100) / 100;
     } else {
         return 0;
@@ -67,29 +67,29 @@ function faixa0(salario) {
     return salario.toFixed(2);
 }
 function faixa75(salario) {
-    salario -= (base * 0.075);
+    salario -= (salario * 0.075);
     return salario.toFixed(2);
 }
 function faixa15(salario) {
     salario -= 53.59;
-    salario -= (base * 0.15);
+    salario -= (salario * 0.15);
     return salario.toFixed(2);
 }
 function faixa225(salario) {
     salario -= 192.25;
-    salario -= (base * 0.225);
+    salario -= (salario * 0.225);
     return salario.toFixed(2);
 }
 function faixa275(salario) {
     salario -= 397.81;
-    salario -= (base * 0.275);
+    salario -= (salario * 0.275);
     return salario.toFixed(2);
 }
 
-function calcDesc(base, salarioFinal) {
+function calcDesc(saldo, salarioFinal) {
     if (salarioFinal > 0) {
-        return (base - salarioFinal).toFixed(2);
+        return (saldo - salarioFinal).toFixed(2);
     } else {
-        return base.toFixed(2);
+        return saldo.toFixed(2);
     }
 }
